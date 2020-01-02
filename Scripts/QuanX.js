95f159fcc7e2e5c06b41755b1004ae7a0a94a689
Long Thinh <longthinhiphone@gmail.com>
2020-01-02 23:24:54 +0700

    Update

--- Scripts/QuanX.js
+++ Scripts/QuanX.js
@@ -1,7 +1,3 @@
-/*
-hostname = raw.githubusercontent.com, *.github.io,
-^https:\/\/(raw.githubusercontent|\w+\.github)\.(com|io)\/.*\.js$ url script-response-body QuanX.js
-*/
 var body = $response.body;
 body = '\/*\n@supported 00DD26467662\n*\/\n' + body;
 $done(body);
