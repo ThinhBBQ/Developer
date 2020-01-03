ab0054b04ae26a4dae139da99cad4d2324e406c1
Long Thinh <longthinhiphone@gmail.com>
2020-01-03 19:04:26 +0700

    Update

--- QuantumultX/QuaX.js
+++ QuantumultX/QuaX.js
@@ -1,6 +1,6 @@
 /*
 hostname = raw.githubusercontent.com, *.github.io,
-^https:\/\/(raw.githubusercontent|\w+\.github)\.(com|io)\/.*\.js$ url script-response-body quanx.js
+^https:\/\/(raw.githubusercontent|\w+\.github)\.(com|io)\/.*\.js$ url script-response-body QuaX.js
 */
 var body = $response.body;
 body = '\/*\n@supported 1AD0C62ADBBC\n*\/\n' + body;
