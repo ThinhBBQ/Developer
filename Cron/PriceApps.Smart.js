cc559aed381e586842c56a6c915be0e913df8c4f
Long Thinh <longthinhiphone@gmail.com>
2020-01-10 22:21:57 +0700

    Update

--- Cron/PriceApps.Smart.js
+++ Cron/PriceApps.Smart.js
@@ -108,7 +108,7 @@ if (isSurge) {
 /*using surge cron*/
 
 const region = "us"
-const appIds = ["804637783",]
+const appIds = ["804637783","517329357"]
 
 var cacheData = $persistentStore.read()
 if (!cacheData) {
