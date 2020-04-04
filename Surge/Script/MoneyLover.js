599a1bda49e3280fff9567136c0ad17eb130c74a
Long Thinh <longthinhiphone@gmail.com>
2020-04-04 14:26:48 +0700

    Update

---
+++ Surge/Script/MoneyLover.js
@@ -0,0 +1,17 @@
+/*
+
+https://revoapi.moneylover.me/api/status
+
+Hostname= revoapi.moneylover.me
+
+*/
+var twicevn = JSON.parse($response.body);
+
+twicevn.data.rwExpire = "9999-10-04T12:59:59.000Z"
+twicevn.data.rwProduct = "finsify_sub_year_1"
+twicevn.data.rwFirstPurchase = "2000-10-04T12:59:59.000Z"
+twicevn.data.purchased = true
+twicevn.data.rwMarket = "apple_store"
+twicevn.data.rwLastPurchase = "9999-10-04T12:59:59.000Z"
+
+$done({body: JSON.stringify(twicevn)});
