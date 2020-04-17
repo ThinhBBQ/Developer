5877e11cf6e54c2d75a0aa7fb254d2c15a44d80a
Long Thinh <longthinhiphone@gmail.com>
2020-04-17 21:51:00 +0700

    Update

--- Surge/Script/DNSPod.js
+++ Surge/Script/DNSPod.js
@@ -1,3 +1,20 @@
+/*
+//DNS
+
+#DNSPod
+dns DNSPod script-path=https://raw.githubusercontent.com/LongThinh/Programer/master/Surge/Script/DNSPod.js,script-update-interval=7200
+
+#Special
+
+*nicegram* = script:DNSPod
+iosnoops.com = script:DNSPod
+ipsw.me = script:DNSPod
+imap.gmail.com = script:DNSPod
+*googleapis.com = script:DNSPod
+*github* = script:DNSPod
+iosapps.itunes.apple.com = script:DNSPod
+
+*/
 const url = 'http://119.29.29.29/d?dn=' + $domain;
 if ($network.v4.primaryInterface === 'en0') {
   $httpClient.get(url, function(error, response, data) {
