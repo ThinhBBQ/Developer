b768ef7435e584b4cbfb1952c767f98143519e9f
Long Thinh <longthinhiphone@gmail.com>
2020-04-17 08:56:47 +0700

    Update

--- PACProfile/Ra1n.js
+++ PACProfile/Ra1n.js
@@ -1,18 +1,23 @@
 var DIRECT = "DIRECT";
-var PROXY = "PROXY 127.0.0.1:7979";
-var blacklist = {"ocsp.apple.com":1,"su.itunes.apple.com":1,"mesu.apple.com":1,"appldnld.apple.com":1,"gdmf.apple.com":1};
+var SURGE = "PROXY 127.0.0.1:6152";
+var BLACK = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080;";
+var blacklist = {
+    "ocsp.apple.com":1,
+    "gdmf.apple.com":1,
+    "mesu.apple.com":1,
+    "swscan.apple.com":1,
+    "su.itunes.apple.com":1,
+    "appldnld.apple.com":1,
+    "world-gen.g.aaplimg.com":1
+};
 function FindProxyForURL(url, host) {
-  host = host.toLowerCase();
-  for (i = 0; i < 30; i++) {
-    if (blacklist[host]) {
-      return PROXY;
-    }
-    var index = host.indexOf(".");
-    if (index == -1) {
-      break;
-    } else {
-      host = host.substring(index + 1);
-    }
-  }
-  return DIRECT;
+    var lastPos;
+    do {
+        if (blacklist.hasOwnProperty(host)) {
+            return BLACK;
+        }
+        lastPos = host.indexOf('.') + 1;
+        host = host.slice(lastPos);
+    } while (lastPos >= 1);
+    return SURGE;
 }
