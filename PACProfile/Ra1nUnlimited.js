08758109e8f7db1c3d849bffde08d7cb53ffed30
Long Thinh <longthinhiphone@gmail.com>
2020-04-17 09:24:06 +0700

    Update

--- PACProfile/Ra1nUnlimited.js
+++ PACProfile/Ra1nUnlimited.js
@@ -1,6 +1,14 @@
 var DIRECT = "DIRECT";
 var PROXY = "PROXY 127.0.0.1:7979";
-var blacklist = {"ocsp.apple.com":1,"su.itunes.apple.com":1,"mesu.apple.com":1,"appldnld.apple.com":1,"gdmf.apple.com":1};
+var blacklist = {
+    "ocsp.apple.com":1,
+    "gdmf.apple.com":1,
+    "mesu.apple.com":1,
+    "swscan.apple.com":1,
+    "su.itunes.apple.com":1,
+    "appldnld.apple.com":1,
+    "world-gen.g.aaplimg.com":1
+};
 function FindProxyForURL(url, host) {
   host = host.toLowerCase();
   for (i = 0; i < 30; i++) {
