31f07cc1bb619c89258b1840d642413bdb1f317d
Long Thinh <longthinhiphone@gmail.com>
2020-04-29 22:57:02 +0700

    Update

--- Scripting/Scripts/Merge/PicsArt.js
+++ Scripting/Scripts/Merge/PicsArt.js
@@ -7,7 +7,4 @@ var obj = JSON.parse($response.body);
 obj.subscription= {
   "granted": true
 };
-obj.is_verified_type= "subscribed";
-obj.is_verified= true;
-obj.is_verified_type_new=true;
 $done({body: JSON.stringify(obj)});
