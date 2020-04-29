f2789abadf532ff402485ab5a153ec9f9085691e
Long Thinh <longthinhiphone@gmail.com>
2020-04-29 23:53:50 +0700

    Update

--- Scripting/Cron/Surge-Cron/v2DataFlow.js
+++ Scripting/Cron/Surge-Cron/v2DataFlow.js
@@ -7,7 +7,7 @@ pass: "Password",
 };
 
 //APILoginMobile
-var body = "account="+account.user + "&build_code=2020.4.15.2&cmnd=&device_id=00000000-0000-0000-0000-000000000000&device_name=%23LongThinh%20iPhone%207%20%28GSM%29%20B%2FAkeyDeviceAcc=xxx&os_type=ios&os_version=13.300000&password="+account.pass + "&version_app=4.3.4";
+var body = "account="+account.user + "&build_code=2020.4.15.2&cmnd=&device_id=00000000-0000-0000-0000-000000000000&device_name=%23LongThinh%20iPhone%207%20%28GSM%29%20B%2FA&keyDeviceAcc=xxx&os_type=ios&os_version=13.300000&password="+account.pass + "&version_app=4.3.4";
 
 var apiloginmobile = {
 url: 'https://apivtp.vietteltelecom.vn:6768/myviettel.php/loginMobile',
