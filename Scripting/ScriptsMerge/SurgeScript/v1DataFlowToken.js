832be10335920384bf2e1f0dc660f22f0ec44fee
Long Thinh <longthinhiphone@gmail.com>
2020-05-01 20:17:10 +0700

    Update

--- Scripting/ScriptsMerge/SurgeScript/v1DataFlowToken.js
+++
@@ -1,9 +0,0 @@
-/*
-hostname = apivtp.vietteltelecom.vn:0
-*/
-
-/*Using Surge Script*/
-let body= $request.body;
-$notification.post("v1DataFlow write cache token: 🎉 Done, success!", "", "")
-$persistentStore.write(body, "bodytoken");
-$done({});
