bfbfb7c47b284df7f5b5f04cf3268b0fa7b8a3a0
Long Thinh <longthinhiphone@gmail.com>
2020-05-07 19:30:04 +0700

    Update

--- Surge/SurgeCron/v2DataFlow.js
+++ Surge/SurgeCron/v2DataFlow.js
@@ -7,7 +7,7 @@ pass: "Password",
 };
 
 //APILoginMobile
-var body = "account="+account.user + "&build_code=2020.4.15.2&cmnd=&device_id=00000000-0000-0000-0000-000000000000&device_name=%23LongThinh%20iPhone%207%20%28GSM%29%20B%2FA&keyDeviceAcc=xxx&os_type=ios&os_version=13.300000&password="+account.pass + "&version_app=4.3.4";
+var body = "account="+account.user + "&build_code=2020.4.15.2&cmnd=&device_id=00000000-0000-0000-0000-000000000000&device_name=%23LongThinh%2C%20iPhone%207&keyDeviceAcc=xxx&os_type=ios&os_version=13.300000&password="+account.pass + "&version_app=4.3.4";
 
 var apiloginmobile = {
 url: 'https://apivtp.vietteltelecom.vn:6768/myviettel.php/loginMobile',
@@ -44,7 +44,7 @@ console.log(data);
 }
 
 function getdataremain(token){ 
-var body = "build_code=2020.4.15.2&device_id=00000000-0000-0000-0000-000000000000&device_name=%23LongThinh%20iPhone%207%20%28GSM%29%20B%2FA&os_type=ios&os_version=13.300000&token=" + token+ "&version_app=4.3.4";
+var body = "build_code=2020.4.15.2&device_id=00000000-0000-0000-0000-000000000000&device_name=%23LongThinh%2C%20iPhone%207&os_type=ios&os_version=13.300000&token=" + token+ "&version_app=4.3.4";
 var dataremain = {
 url: 'https://apivtp.vietteltelecom.vn:6768/myviettel.php/getDataRemain',
 headers: {},
@@ -60,9 +60,6 @@ let obj= JSON.parse(data);
 if(obj["errorCode"] === "0"){
 var data= obj["data"][0];
 $notification.post("❀ Cellular (2G/3G/LTE): " + data["pack_name"], "",  "✮ Available/Remain: " + data["remain_mb"]+"MB ~ " + Math.round(data["remain_mb"]/1024) + "GB\n❀ Expire date: " + data["expireDate"]);
-}
-else{
-$notification.post("Token expired", "Re-Login in the My Viettel app", "Please!");
 }
 }
 }
