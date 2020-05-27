78c2366dc6ba44477b21ac2245b5d8fd3be73d28
 Long Thinh <longthinhiphone@gmail.com>
2020-05-27 20:14:02 +0700

    Update

--- Surge/SurgeScript/v1DataFlowToken.js
+++
@@ -1,9 +0,0 @@
-/*
-hostname = apivtp.vietteltelecom.vn:0
-*/
-
-/*Using Surge Script*/
-let body= $request.body;
-$notification.post("", "Rewrite token: 🎉 Done, success!", "")
-$persistentStore.write(body, "bodytoken");
-$done({});
