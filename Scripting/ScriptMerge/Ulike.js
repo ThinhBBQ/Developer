90d3f856dcb996d9709702925533b7006a7d4b10
 Long Thinh <longthinhiphone@gmail.com>
2020-06-02 05:33:32 +0700

    Update

--- Scripting/ScriptMerge/Ulike.js
+++ Scripting/ScriptMerge/Ulike.js
@@ -1,14 +1,7 @@
-var obj = JSON.parse($response.body);
+var hcsavn = JSON.parse($response.body);
 
-obj= {
-  "data": {
-    "flag": true,
-    "start_time": 1572760027,
-    "end_time": 4097368706
-  },
-  "systime": "",
-  "errmsg": "Success",
-  "ret": "0"
-};
+hcsavn.data.start_time = 1591022354
+hcsavn.data.end_time = 253394611200
+hcsavn.data.flag = true
 
-$done({body: JSON.stringify(obj)});
+$done({body: JSON.stringify(hcsavn)});
