a86988f4e0eb2150c44ddcd4e8bb22c23224c56d
 Long Thinh <longthinhiphone@gmail.com>
2020-06-05 06:47:26 +0700

    Update

--- QuantumultX/QxCron/DataFlow.js
+++ QuantumultX/QxCron/DataFlow.js
@@ -113,7 +113,7 @@ pass: "Password",
 };
 
 //APILoginMobile
-var body = "account="+account.user + "&build_code=2020.4.15.2&cmnd=&device_id=00000000-0000-0000-0000-000000000000&device_name=iPhone%207%20%28%20Long%20Thinh%29&keyDeviceAcc=xxx&os_type=ios&os_version=13.300000&password="+account.pass + "&version_app=4.3.4";
+var body = "account="+account.user + "&build_code=2020.4.15.2&cmnd=&device_id=00000000-0000-0000-0000-000000000000&device_name=iPhone%207%20%28Long%20Thinh%29&keyDeviceAcc=xxx&os_type=ios&os_version=13.300000&password="+account.pass + "&version_app=4.3.4";
 
 var apiloginmobile = {
 url: 'https://apivtp.vietteltelecom.vn:6768/myviettel.php/loginMobile',
@@ -149,7 +149,7 @@ $notification.post("", "Login failed or the token has expired", "Trying to Re-Lo
 }
 
 function getdataremain(token){ 
-var body = "build_code=2020.4.15.2&device_id=00000000-0000-0000-0000-000000000000&device_name=iPhone%207%20%28%20Long%20Thinh%29&os_type=ios&os_version=13.300000&token=" + token+ "&version_app=4.3.4";
+var body = "build_code=2020.4.15.2&device_id=00000000-0000-0000-0000-000000000000&device_name=iPhone%207%20%28Long%20Thinh%29&os_type=ios&os_version=13.300000&token=" + token+ "&version_app=4.3.4";
 var dataremain = {
 url: 'https://apivtp.vietteltelecom.vn:6768/myviettel.php/getDataRemain',
 headers: {},
