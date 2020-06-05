3469bed413b2e40910b3041b04c42db1cc15fcd0
 Long Thinh <longthinhiphone@gmail.com>
2020-06-05 19:27:29 +0700

    Update

--- Scripting/ScriptMerge/RECGO.js
+++ Scripting/ScriptMerge/RECGO.js
@@ -1,8 +1,8 @@
 {
   "status": "success",
   "data": {
-    "country_code": "VN",
-    "user_id": 13136953,
+    "country_code": "US",
+    "user_id": 13200594,
     "expires_date_ms": 4102376399000,
     "user_type": "vip"
   }
