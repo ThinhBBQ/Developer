1acf8c7730cb9705fae84294e5af26d79a79e457
 Long Thinh <longthinhiphone@gmail.com>
2020-06-15 22:02:00 +0700

    Update

--- Surge/SurgeCron/Apprice.js
+++ Surge/SurgeCron/Apprice.js
@@ -23,11 +23,11 @@ $httpClient.post('https://itunes.apple.com/lookup?id=' + appIds + "&country=" + 
         for (var i = 0; i < appData.length; i++) {
             if (cacheData[appData[i].trackId]) {
                 if (appData[i].formattedPrice != cacheData[appData[i].trackId].price) {
-                    priceChanged = priceChanged + "💵 " + appData[i].trackName + "  " + cacheData[appData[i].trackId].price + " → " + appData[i].formattedPrice + "\n"
+                    priceChanged = priceChanged + "💵 " + appData[i].trackName + " ✮ " + cacheData[appData[i].trackId].price + " → " + appData[i].formattedPrice + "\n"
                     cacheData[appData[i].trackId].price = appData[i].formattedPrice
                 }
             } else {
-                newAppAdded = newAppAdded + "💵 " + appData[i].trackName + "  " + appData[i].formattedPrice + "\n"
+                newAppAdded = newAppAdded + "💵 " + appData[i].trackName + " ✮ " + appData[i].formattedPrice + "\n"
                 cacheData[appData[i].trackId] = {
                     name: appData[i].trackName,
                     price: appData[i].formattedPrice
