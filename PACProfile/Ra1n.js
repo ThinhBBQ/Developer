104d8154c3562671cf6f0b1f3e2f60d1432c9c86
 Long Thinh <longthinhiphone@gmail.com>
2020-08-12 18:33:27 +0700

    Update

--- PACProfile/Ra1n.js
+++ PACProfile/Ra1n.js
@@ -3,6 +3,7 @@ var PROXY = "PROXY 127.0.0.1:7979";
 var blacklist = {
     "ocsp.apple.com":1,
     "world-gen.g.aaplimg.com":1,
+    "appldnld.apple.com":1,
     "su.itunes.apple.com":1,
     "mesu.apple.com":1,
     "gdmf.apple.com":1,
