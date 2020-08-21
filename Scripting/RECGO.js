77a21af493538caa6a5bbfd1c43fd8d94a89b1d7
 Long Thinh <longthinhiphone@gmail.com>
2020-08-21 20:23:41 +0700

    Update

--- Scripting/RECGO.js
+++
@@ -1,9 +0,0 @@
-{
-  "status": "success",
-  "data": {
-    "country_code": "US",
-    "user_id": 13200594,
-    "expires_date_ms": 4102376399000,
-    "user_type": "vip"
-  }
-}
--- Scripting/Ulike.js
+++
@@ -1,9 +0,0 @@
-/* hostname = commerce-i18n-api.faceu.mobi */
-
-var hcsavn = JSON.parse($response.body);
-
-hcsavn.data.start_time = 1591022354
-hcsavn.data.end_time = 4102419599
-hcsavn.data.flag = true
-
-$done({body: JSON.stringify(hcsavn)});
