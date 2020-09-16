ccdd2e9fa45fbdf57bdba44b510a69b74f159e0e
 Long Thinh <longthinhiphone@gmail.com>
2020-09-16 19:25:01 +0700

    Update

--- Scripting/Apprice.js
+++ Scripting/Apprice.js
@@ -1,7 +1,131 @@
-/* Only for Surge
-Using Surge Cron */
+//Smartcode
+let isQuantumultX = $task != undefined; 
+let isSurge = $httpClient != undefined; 
+//HTTP
+var $task = isQuantumultX ? $task : {};
+var $httpClient = isSurge ? $httpClient : {};
+//Cookie
+var $prefs = isQuantumultX ? $prefs : {};
+var $persistentStore = isSurge ? $persistentStore : {};
+var $notify = isQuantumultX ? $notify : {};
+var $notification = isSurge ? $notification : {};
+//Endregion
+//Region 
+if (isQuantumultX) {
+    var errorInfo = {
+        error: ''
+    };
+    $httpClient = {
+        get: (url, cb) => {
+            var urlObj;
+            if (typeof (url) == 'string') {
+                urlObj = {
+                    url: url
+                }
+            } else {
+                urlObj = url;
+            }
+            $task.fetch(urlObj).then(response => {
+                cb(undefined, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        },
+        post: (url, cb) => {
+            var urlObj;
+            if (typeof (url) == 'string') {
+                urlObj = {
+                    url: url
+                }
+            } else {
+                urlObj = url;
+            }
+            url.method = 'POST';
+            $task.fetch(urlObj).then(response => {
+                cb(undefined, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        }
+    }
+}
+if (isSurge) {
+    $task = {
+        fetch: url => {
+            return new Promise((resolve, reject) => {
+                if (url.method == 'POST') {
+                    $httpClient.post(url, (error, response, data) => {
+                        if (response) {
+                            response.body = data;
+                            resolve(response, {
+                                error: error
+                            });
+                        } else {
+                            resolve(null, {
+                                error: error
+                            })
+                        }
+                    })
+                } else {
+                    $httpClient.get(url, (error, response, data) => {
+                        if (response) {
+                            response.body = data;
+                            resolve(response, {
+                                error: error
+                            });
+                        } else {
+                            resolve(null, {
+                                error: error
+                            })
+                        }
+                    })
+                }
+            })
+
+        }
+    }
+}
+//Endregion
+//Region cookie
+if (isQuantumultX) {
+    $persistentStore = {
+        read: key => {
+            return $prefs.valueForKey(key);
+        },
+        write: (val, key) => {
+            return $prefs.setValueForKey(val, key);
+        }
+    }
+}
+if (isSurge) {
+    $prefs = {
+        valueForKey: key => {
+            return $persistentStore.read(key);
+        },
+        setValueForKey: (val, key) => {
+            return $persistentStore.write(val, key);
+        }
+    }
+}
+//Endregion
+//Region 
+if (isQuantumultX) {
+    $notification = {
+        post: (title, subTitle, detail) => {
+            $notify(title, subTitle, detail);
+        }
+    }
+}
+if (isSurge) {
+    $notify = function (title, subTitle, detail) {
+        $notification.post(title, subTitle, detail);
+    }
+}
+//Endregion
 const region = "us"
-const appIds = ["1292141449","533054060","1450650398","1392434975","840190547","1483522561","951598770","1461666739","1275979443","1283082051","1317522797","388624839","1291222612","1067596534","1479572902","517329357","1515743741","1488691677","1493095946","961390574","1488149884"]
+const appIds = ["1435082414","1292141449","533054060","1450650398","1392434975","840190547","1483522561","951598770","1461666739","1275979443","1283082051","1317522797","388624839","1291222612","1067596534","1479572902","517329357","1515743741","1488691677","1493095946","961390574","1488149884"]
 
 var cacheData = $persistentStore.read()
 if (!cacheData) {
