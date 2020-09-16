b1c0042a01efef355d01ec4b1b052b88a29d8a23
 Long Thinh <longthinhiphone@gmail.com>
2020-09-16 19:40:22 +0700

    Update

--- Scripting/CamScaner.js
+++
@@ -1,8 +0,0 @@
-/*
-hostname = ap*.intsig.net
-*/
-
-/*Using Surge & Quantumult X Script*/
-let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"4073275762"}}};
-$done({body: JSON.stringify(obj)});
--- Scripting/Textnow.js
+++
@@ -1,9 +0,0 @@
-/*
-hostname = api.textnow.me
-*/
-
-/*Using Surge & Quantumult X Script*/
-var obj = JSON.parse($response.body); 
-obj['show_ads'] = false;
-obj['premium_calling'] = true;
-$done({body: JSON.stringify(obj)});
