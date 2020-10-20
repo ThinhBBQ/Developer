8fef3f0543f940aed9b7129f6fb6dafc97b3c9f2
 Long Thinh <longthinhiphone@gmail.com>
2020-10-20 22:27:38 +0700

    Update

--- PACProfile/PACRa1n.js
+++ PACProfile/PACRa1n.js
@@ -5,6 +5,7 @@ var blacklist = {
     "su.itunes.apple.com":1,
     "ocsp.apple.com";1,
     "gdmf.apple.com":1,
+    "xp.apple.com":1,
 };
 function FindProxyForURL(url, host) {
   host = host.toLowerCase();
