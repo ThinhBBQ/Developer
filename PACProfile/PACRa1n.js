4e9af09fdc05042f4a3da059d016df632e5fd6ba
 Leͥgeͣnͫd <longthinhiphone@gmail.com>
2020-12-17 17:38:05 +0700

    Update

--- PACProfile/PACRa1n.js
+++ PACProfile/PACRa1n.js
@@ -1,5 +1,5 @@
 var DIRECT = "PROXY 127.0.0.1:6152; DIRECT";
-var PROXY = "PROXY 127.0.0.1:1080";
+var PROXY = "PROXY 127.0.0.1:6153";
 var blacklist = {
     "su.itunes.apple.com":1,
     "gdmf.apple.com":1,
